6f01a84c66d6e380c54e7179554d43373e9b6646
jorber <berntsen.jorgen@gmail.com>
2022-10-05 22:11:51 +0200

    Endret på searchknapp design

--- aksjeapp-frontend/clientapp/src/components/UI/SearchBar/SearchBar.tsx
+++ aksjeapp-frontend/clientapp/src/components/UI/SearchBar/SearchBar.tsx
@@ -7,10 +7,11 @@ type Props = {
 const SearchBar: React.FC<Props> = () => {
     return (
         <>
-            <div className="flex justify-between rounded-2xl drop-shadow-2xl bg-gray-50 shadow-inner h-16 pl-5 p-2">
+            <div
+                className="flex justify-between rounded-2xl drop-shadow-2xl bg-gray-50 shadow-inner pl-5 overflow-hidden">
                 <input className="w-80" style={{outline: "none"}} type="text"/>
-                <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold py-2 px-4 border
-                border-blue-500 hover:border-transparent rounded-2xl hover:text-white transition duration-300 ease-in-out">
+                <button
+                    className="bg-transparent hover:bg-gray-300 text-blue-700 font-semibold py-2 px-4 hover:text-black transition duration-300 ease-in-out">
                     Search
                 </button>
             </div>

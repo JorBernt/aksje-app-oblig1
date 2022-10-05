9059b401deacaa3a5a2075798b7fe2795f44d9e0
jorber <berntsen.jorgen@gmail.com>
2022-10-05 16:17:59 +0200

    Videre arbeid på components for LandingPage

---
+++ aksjeapp-frontend/clientapp/src/components/UI/Card/Card.tsx
@@ -0,0 +1,21 @@
+import React, {FC} from 'react';
+
+type Props = {
+    color: string;
+    children : React.ReactNode;
+}
+
+const Card : FC<Props> = (props ) => {
+    const className = "drop-shadow-md rounded-2xl p-5 m-10 " + (props.color === "default" ? "bg-gray-500" : props.color);
+    return (
+        <>
+            <div className={className}>
+                {props.children}
+            </div>
+        </>
+    )
+}
+
+export default Card;
+
+
--- aksjeapp-frontend/clientapp/src/components/pages/LandingPage.tsx
+++ aksjeapp-frontend/clientapp/src/components/pages/LandingPage.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import Header from "../Header";
+import Card from "../UI/Card/Card";
+import Navbar from "../Navbar/Navbar";
 
 const LandingPage = () => {
     return (
         <>
-            <Header/>
+            <Navbar/>
+            <div className="flex flex-row justify-between">
+                <div className="basis-1">
+                    <div className="flex flex-auto">
+                        <Card color={"default"}>
+                            <p className="text-5xl">Hello World!</p>
+                        </Card>
+
+                    </div>
+                </div>
+                <div className="basis-1">
+                    <div className="flex flex-col">
+                        <Card color="default">
+                            <p className="text-5xl">Card 2</p>
+                        </Card>
+                        <Card color="default">
+                            <p className="text-5xl">Card 3</p>
+                        </Card>
+
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

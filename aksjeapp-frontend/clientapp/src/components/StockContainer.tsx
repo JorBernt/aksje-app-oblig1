1eff9c29e43e3883395797ed20597cefdb1c154c
JuanMephisto <adrian@grotter.net>
2022-10-06 01:16:34 +0200

    små endringer på datasettet

--- aksjeapp-frontend/clientapp/src/components/StockContainer.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockContainer.tsx
@@ -8,13 +8,13 @@ type Props = {
 const StockContainer = (props: Props) => {
 
     const data = [{id: 0, name: "AAPL", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 1, name: "AMZN", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 2, name: "TSLA", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 3, name: "MSFT", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 4, name: "GOOGL", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 5, name: "BRK.A", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 6, name: "NVDA", chart: "chart", difference: 2.56, value: 100.45},
-                {id: 7, name: "TSM", chart: "chart", difference: 2.56, value: 100.45}];
+                {id: 1, name: "AMZN", chart: "chart", difference: -1.09, value: 80.10},
+                {id: 2, name: "TSLA", chart: "chart", difference: 3.48, value: 90.81},
+                {id: 3, name: "MSFT", chart: "chart", difference: 30.91, value: 211.21},
+                {id: 4, name: "GOOGL", chart: "chart", difference: 2.45, value: 82.45},
+                {id: 5, name: "BRK.A", chart: "chart", difference: -1.80, value: 89.92},
+                {id: 6, name: "NVDA", chart: "chart", difference: 2.10, value: 34.90},
+                {id: 7, name: "TSM", chart: "chart", difference: 12.22, value: 12.60}];
     return(
         <>
             <div className="justify-center flex-row px-0">

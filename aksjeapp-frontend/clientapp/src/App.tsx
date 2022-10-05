3c66d18dd92500fedc795f6ce032d6f22b113a49
jorber <berntsen.jorgen@gmail.com>
2022-10-05 11:16:57 +0200

    Lagt til start på header for å teste tailwind

--- aksjeapp-frontend/clientapp/src/App.tsx
+++ aksjeapp-frontend/clientapp/src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
-fetch('https://localhost:7187/Example/Example',{method: "get"}).then(res => res.json().then(res => console.log(res)))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Header/>
+      </>
   );
 }
 

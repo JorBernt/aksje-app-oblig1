d35c7f97df799180ce7b3ee2a4e22b13ab04b77e
JorBernt <berntsen.jorgen@gmail.com>
2022-10-06 17:21:23 +0200

    Flyttet "SingleStockViewer" til egen component

--- aksjeapp-frontend/clientapp/src/components/Pages/LandingPage.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/LandingPage.tsx
@@ -1,21 +1,38 @@
 import React from 'react';
 import Card from "../UI/Card/Card";
 import Navbar from "../Navbar/Navbar";
-import Chart from "../UI/Chart/Chart";
 import StockContainer from "../StockContainer";
+import SingleStockView from "../StockViews/SingleStockView";
 
 const LandingPage = () => {
     document.body.style.backgroundColor = "black"
+    const stockData = {
+        "name": "Porsche AG",
+        "last": 90.78,
+        "todayPercent": "+3.25%",
+        "todayDifference": "+2.86",
+        "buy": 90.78,
+        "sell": 90.80,
+        "high": 93.70,
+        "low": 89.50,
+        "turnover": 1386098
+    };
+    let rowNames = new Map([
+        ["Last", "last"],
+        ["Today %", "todayPercent"],
+        ["Today +/-", "todayDifference"],
+        ["Buy", "buy"],
+        ["Sell", "sell"],
+        ["High", "high"],
+        ["Low", "low"],
+        ["Turnover", "turnover"]]);
     return (
         <>
             <Navbar/>
             <div className="flex flex-row justify-center mt-5">
                 <div className="basis-1">
                     <div className="flex flex-auto">
-                        <Card color={"default"}>
-                            <p className="text-5xl text-center pb-5">Aksje</p>
-                            <Chart/>
-                        </Card>
+                        <SingleStockView stockData={stockData} rowNames={rowNames}/>
                     </div>
                 </div>
                 <div className="basis-1">

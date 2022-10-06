7f486563603b69c6e870e2f53e50eadabaed8751
JuanMephisto <adrian@grotter.net>
2022-10-06 17:31:40 +0200

    Renamed component. Added title to chart

--- aksjeapp-frontend/clientapp/src/components/Pages/ProfilePage.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/ProfilePage.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Card from "../UI/Card/Card";
-import ProfileInfoBox from "../ProfileInfoBox";
+import ProfileInfoContainer from "../ProfileComponents/ProfileInfoContainer";
 import Chart from "../UI/Chart/Chart";
 
 const ProfilePage = () => {
@@ -9,15 +9,17 @@ const ProfilePage = () => {
         <>
             <div>
                 <Navbar/>
+                <h1 className="text-center text-4xl pt-10 pb-5">Your profile</h1>
                 <div className="flex flex-row justify-center">
                     <div className="w-96">
                         <Card color={"default"}>
-                            <ProfileInfoBox/>
+                            <ProfileInfoContainer/>
                         </Card>
 
                     </div>
                     <div>
                         <Card color={"default"}>
+                            <p className="text-3xl text-center pb-5 text-black">Your best stock this week!</p>
                             <Chart/>
                         </Card>
                     </div>

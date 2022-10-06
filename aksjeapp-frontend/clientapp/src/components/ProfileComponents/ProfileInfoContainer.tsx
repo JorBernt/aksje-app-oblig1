89d081c020f99e0ad8b7ad6a084b206fd2719ede
JuanMephisto <adrian@grotter.net>
2022-10-06 22:27:29 +0200

    colors

--- aksjeapp-frontend/clientapp/src/components/ProfileComponents/ProfileInfoContainer.tsx
+++ aksjeapp-frontend/clientapp/src/components/ProfileComponents/ProfileInfoContainer.tsx
@@ -5,13 +5,13 @@ const ProfileInfoContainer = () => {
         <>
             <div className="grid-cols-1">
                 <h1 className="flex justify-center py-1 text-text-display">Mephisto</h1>
-                <hr className="border-black"/>
+                <hr className="border-text-display"/>
                 <ProfileTextDisplay title="Penger på konto" content="1500$"/>
                 <ProfileTextDisplay title="Portfolio Value" content="214390.23$"/>
                 <ProfileTextDisplay title="Change this week" content="+0.09%"/>
                 <ProfileTextDisplay title="Penger på konto" content="1500$"/>
                 <ProfileTextDisplay title="Portfolio Value" content="214390.23$"/>
-                <hr className="border-black"/>
+                <hr className="border-text-display"/>
                 <ProfileTextDisplay title="Your stocks" content="AAPL, AMZN, TSM, NVDA, TSLA, BRK.A, MSFT, GOOGL"/>
 
             </div>

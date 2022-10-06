3e1f817d7ea26480fcc71de724c6f29cfe542288
JuanMephisto <adrian@grotter.net>
2022-10-06 13:11:44 +0200

    improved ternary if; more readable

--- aksjeapp-frontend/clientapp/src/components/StockPreview.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockPreview.tsx
@@ -13,12 +13,14 @@ type Props = {
 }
 
 const StockPreview : FC<Props> = (props: Props) => {
-
+    const redOrGreen = props.items.difference > 0  ?
+        <p className="py-1 text-green-500">{"+"+props.items.difference+"%"}</p> :
+        <p className="py-1 text-red-600">{props.items.difference+"%"}</p>
     return(
         <div className="grid grid-cols-4 w-96">
             <p className="py-1 pl-4">{props.items.name}</p>
             <p className="py-1">{props.items.chart}</p>
-            {props.items.difference > 0  ? <p className="py-1 text-green-500">{"+"+props.items.difference+"%"}</p> : <p className="py-1 text-red-600">{props.items.difference+"%"}</p>}
+            { redOrGreen }
             <p className="py-1 pr-4">{"$"+props.items.value}</p>
         </div>
     );

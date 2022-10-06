d2cfb1fb58fdf33627ef51161052ed2681ba44f9
JuanMephisto <adrian@grotter.net>
2022-10-06 22:25:27 +0200

    updated the test-dataset

--- aksjeapp-frontend/clientapp/src/components/StockViews/StockContainer.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockViews/StockContainer.tsx
@@ -8,22 +8,22 @@ type Props = {
 }
 
 const StockContainer = (props: Props) => {
-    const data = [{id: 0, name: "AAPL", amount: 100, chart: "chart", difference: 2.56, value: 100.45},
-        {name: "AMZN", amount: 100, chart: "chart", difference: -1.09, value: 80.10},
-        {name: "TSLA", amount: 100, chart: "chart", difference: 3.48, value: 90.81},
-        {name: "MSFT", amount: 100, chart: "chart", difference: 30.91, value: 211.21},
-        {name: "GOOGL", amount: 100, chart: "chart", difference: 2.45, value: 82.45},
-        {name: "BRK.A", amount: 100, chart: "chart", difference: -1.80, value: 89.92},
-        {name: "NVDA", amount: 100, chart: "chart", difference: 2.10, value: 34.90},
-        {name: "TSM", amount: 100, chart: "chart", difference: 12.22, value: 12.60},
-        {name: "AAPL", amount: 100, chart: "chart", difference: 2.56, value: 100.45},
-        {name: "AMZN", amount: 100, chart: "chart", difference: -1.09, value: 80.10},
-        {name: "TSLA", amount: 100, chart: "chart", difference: 3.48, value: 90.81},
-        {name: "MSFT", amount: 100, chart: "chart", difference: 30.91, value: 211.21},
-        {name: "GOOGL", amount: 100, chart: "chart", difference: 2.45, value: 82.45},
-        {name: "BRK.A", amount: 100, chart: "chart", difference: -1.80, value: 89.92},
-        {name: "NVDA", amount: 100, chart: "chart", difference: 2.10, value: 34.90},
-        {name: "TSM", amount: 100, chart: "chart", difference: 12.22, value: 12.60}];
+    const data = [{id: 0, name: "TCEHY", amount: 950, chart: "chart", difference: 4.01, value: 60.29},
+        {name: "AMZN", amount: 120, chart: "chart", difference: -1.09, value: 80.10},
+        {name: "TSLA", amount: 100000, chart: "chart", difference: 3.48, value: 90.81},
+        {name: "MSFT", amount: 50, chart: "chart", difference: 30.91, value: 211.21},
+        {name: "GOOGL", amount: 1500, chart: "chart", difference: 2.45, value: 82.45},
+        {name: "BRK-B", amount: 1700, chart: "chart", difference: -1.80, value: 89.92},
+        {name: "NVDA", amount: 800, chart: "chart", difference: 2.10, value: 34.90},
+        {name: "TSM", amount: 500, chart: "chart", difference: 12.22, value: 12.60},
+        {name: "AAPL", amount: 200, chart: "chart", difference: 2.56, value: 100.45},
+        {name: "2222.SR", amount: 500, chart: "chart", difference: -2.08, value: 80.10},
+        {name: "UNH", amount: 900, chart: "chart", difference: -2.48, value: 90.81},
+        {name: "JNJ", amount: 1000, chart: "chart", difference: -4.72, value: 211.21},
+        {name: "XOM", amount: 1000, chart: "chart", difference: 2.28, value: 82.45},
+        {name: "V", amount: 10000, chart: "chart", difference: -9.30, value: 89.92},
+        {name: "META", amount: 83000, chart: "chart", difference: 2.90, value: 34.90},
+        {name: "WMT", amount: 9000, chart: "chart", difference: 15.32, value: 12.60}];
 
 
     let sorted;

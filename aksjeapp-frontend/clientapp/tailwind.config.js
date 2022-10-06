6bef92a87fa677c46e358d48dd4f2ceba2c7e9c6
JuanMephisto <adrian@grotter.net>
2022-10-06 14:40:18 +0200

    Added custom colors

--- aksjeapp-frontend/clientapp/tailwind.config.js
+++ aksjeapp-frontend/clientapp/tailwind.config.js
@@ -1,10 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+      'card': '#ffffff',
+      'navbar': '#ffffff',
+      'background': "#eeeeee",
+      'gray': colors.gray,
+      'black': colors.black,
+      'red': colors.red,
+      'green': colors.green,
+      'blue': colors.blue,
+    },
   },
   plugins: [],
 }

05972542fcd0402b7c341a251c7c4552c83177b1
JuanMephisto <adrian@grotter.net>
2022-10-06 22:03:08 +0200

    made number of columns in stockPreview dynamic

--- aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
@@ -18,8 +18,10 @@ const StockPreview : FC<Props> = (props: Props) => {
         <p className="py-1 text-green-500 text-center">{"+" + props.items.difference + "%"}</p> :
         <p className="py-1 text-red-500 text-center">{props.items.difference + "%"}</p>
     const ifAmount = props.showAmount ? <p className="py-1 text-center">{props.items.amount}</p> : null
+
+    const cols = "grid w-96 " + (props.showAmount ? "grid-cols-5" : "grid-cols-4")
     return (
-        <div className="grid grid-cols-5 w-96">
+        <div className={cols}>
             <p className="py-1 pl-4 text-center">{props.items.name}</p>
             {ifAmount}
             <p className="py-1 text-center">{props.items.chart}</p>

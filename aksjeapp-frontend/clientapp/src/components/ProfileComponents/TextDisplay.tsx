1ce68231c916882e2e45d75067e889ec4c030717
JuanMephisto <adrian@grotter.net>
2022-10-07 11:14:38 +0200

    made textdisplay dynamic

--- aksjeapp-frontend/clientapp/src/components/ProfileComponents/TextDisplay.tsx
+++ aksjeapp-frontend/clientapp/src/components/ProfileComponents/TextDisplay.tsx
@@ -1,6 +1,6 @@
 type Props = {
-    title: string;
-    content: string
+    title?: string;
+    content?: string
 }
 
 const TextDisplay = (props: Props) => {
@@ -8,8 +8,8 @@ const TextDisplay = (props: Props) => {
     return (
         <>
             <div className="pt-2.5 pb-2.5 text-text-display">
-                <h2 className="text-center font-semibold">{props.title}</h2>
-                <p className="text-center">{props.content}</p>
+                {props.title == null ? null : <h2 className="text-center font-semibold">{props.title}</h2>}
+                {props.content == null ? null : <p className="text-center">{props.content}</p>}
             </div>
         </>
     )

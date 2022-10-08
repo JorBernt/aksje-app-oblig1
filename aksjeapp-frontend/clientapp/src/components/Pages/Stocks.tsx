c658c636da9f4aa2a15dcb85c809a0b55d8ab4bf
JuanMephisto <adrian@grotter.net>
2022-10-08 17:32:40 +0200

    Continued work on Stocks.tsx

--- aksjeapp-frontend/clientapp/src/components/Pages/Stocks.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/Stocks.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../Navbar/Navbar";
 import Card from "../UI/Card/Card";
 import StockContainer from "../StockViews/StockContainer";
+import NewsContainer from "../NewsContainer";
 
 const Stocks = () => {
     return (
@@ -8,10 +9,17 @@ const Stocks = () => {
             <div>
                 <Navbar/>
                 <h1 className="text-center font-semi text-5xl bold py-10">All Stocks</h1>
-                <div className="flex justify-center">
-                    <Card>
-                        <StockContainer text="All stocks" showAmount={false} sorted="valAsc"/>
-                    </Card>
+                <div className="flex flex-row justify-center">
+                    <div className="flex justify-center">
+                        <Card>
+                            <StockContainer text="All stocks" showAmount={false} sorted="valAsc"/>
+                        </Card>
+                    </div>
+                    <div>
+                        <Card>
+                            <NewsContainer/>
+                        </Card>
+                    </div>
                 </div>
             </div>
         </>

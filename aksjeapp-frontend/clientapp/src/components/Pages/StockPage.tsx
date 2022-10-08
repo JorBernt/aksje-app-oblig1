6c33335005f2873251401eaff0a31ed2be8bb000
JuanMephisto <adrian@grotter.net>
2022-10-08 10:55:34 +0200

    Created StockPage.tsx

---
+++ aksjeapp-frontend/clientapp/src/components/Pages/StockPage.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/Navbar";
+
+
+const StockPage = () => {
+    return (
+        <>
+            <div>
+                <Navbar/>
+                <h1>This page will show indepth about a stock and let you buy and sell.</h1>
+            </div>
+        </>
+    )
+}
+
+export default StockPage;

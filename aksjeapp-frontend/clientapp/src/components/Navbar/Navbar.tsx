6a12c9f5208c089b3ec93ffe10051a1fd6c6a83d
JuanMephisto <adrian@grotter.net>
2022-10-08 10:21:15 +0200

    Translated everything to english

--- aksjeapp-frontend/clientapp/src/components/Navbar/Navbar.tsx
+++ aksjeapp-frontend/clientapp/src/components/Navbar/Navbar.tsx
@@ -8,15 +8,15 @@ function Navbar() {
         <>
             <nav className="flex items-center justify-between bg-navbar border-b-2 border-black p-1">
                 <a href="/" className="text-2xl ml-10">
-                    Aksjehandel
+                    Stock Trading
                 </a>
                 <SearchBar/>
                 <div className="flex items-center justify-between">
-                    <NavbarLink link={"/"} text={"Hjem"}/>
+                    <NavbarLink link={"/"} text={"Home"}/>
                     <Divider/>
-                    <NavbarLink link={"/stocks"} text={"Aksjer"}/>
+                    <NavbarLink link={"/stocks"} text={"Stocks"}/>
                     <Divider/>
-                    <NavbarLink link={"/profile"} text={"Profil"}/>
+                    <NavbarLink link={"/profile"} text={"Profile"}/>
                 </div>
             </nav>
         </>

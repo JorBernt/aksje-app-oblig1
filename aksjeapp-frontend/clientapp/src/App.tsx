3148069b693dc4da39f7b0c924b7a69e0e920f4d
JuanMephisto <adrian@grotter.net>
2022-10-08 10:55:34 +0200

    Created StockPage.tsx

--- aksjeapp-frontend/clientapp/src/App.tsx
+++ aksjeapp-frontend/clientapp/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import LandingPage from "./components/Pages/LandingPage";
 import ProfilePage from "./components/Pages/ProfilePage";
+import StockPage from "./components/Pages/StockPage";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Routes>
                 <Route path="/" element={<LandingPage/>}/>
                 <Route path="/profile" element={<ProfilePage/>}/>
+                <Route path="/stocks" element={<StockPage/>}/>
             </Routes>
         </Router>
     );

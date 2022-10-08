63eb54ed8eb3f2fd817b5547991f3530bf337bc2
JuanMephisto <adrian@grotter.net>
2022-10-08 11:01:54 +0200

    More work on StockPage.tsx

--- aksjeapp-frontend/clientapp/src/components/Pages/StockPage.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/StockPage.tsx
@@ -1,12 +1,33 @@
 import Navbar from "../Navbar/Navbar";
+import SingleStockView from "../StockViews/SingleStockView";
+import Card from "../UI/Card/Card";
 
+const stockData = {
+    "name": "Porsche AG",
+    "last": 90.78,
+    "todayPercent": "+3.25%",
+    "todayDifference": "-1.86",
+    "buy": 90.78,
+    "sell": 90.80,
+    "high": 93.70,
+    "low": 89.50,
+    "turnover": 1386098
+};
 
 const StockPage = () => {
     return (
         <>
             <div>
                 <Navbar/>
-                <h1>This page will show indepth about a stock and let you buy and sell.</h1>
+                <h1 className="text-center font-semi text-5xl bold py-10">Stocks</h1>
+                <div className="flex justify-center">
+                    <SingleStockView stockData={stockData}/>
+                </div>
+                <div className="flex justify-center">
+                    <Card>
+                        <h1 className="text-center font-semibold">Buy and Sell buttons</h1>
+                    </Card>
+                </div>
             </div>
         </>
     )

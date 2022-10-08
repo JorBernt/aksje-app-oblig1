33f868606a5d4e7c44f2b6ab35d6eadc633111f2
JuanMephisto <adrian@grotter.net>
2022-10-08 10:21:15 +0200

    Translated everything to english

--- aksjeapp-frontend/clientapp/src/components/ProfileComponents/ProfileInfoContainer.tsx
+++ aksjeapp-frontend/clientapp/src/components/ProfileComponents/ProfileInfoContainer.tsx
@@ -6,10 +6,10 @@ const ProfileInfoContainer = () => {
             <div className="grid-cols-1">
                 <h1 className="flex justify-center py-1 text-text-display">Mephisto</h1>
                 <hr className="border-text-display"/>
-                <TextDisplay title="Penger på konto" content="1500$"/>
+                <TextDisplay title="Unspent money" content="1500$"/>
                 <TextDisplay title="Portfolio Value" content="214390.23$"/>
                 <TextDisplay title="Change this week" content="+0.09%"/>
-                <TextDisplay title="Penger på konto" content="1500$"/>
+                <TextDisplay title="Unspent money" content="1500$"/>
                 <TextDisplay title="Portfolio Value" content="214390.23$"/>
                 <hr className="border-text-display"/>
                 <TextDisplay title="Your stocks" content="AAPL, AMZN, TSM, NVDA, TSLA, BRK.A, MSFT, GOOGL"/>

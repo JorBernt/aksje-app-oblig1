014e0d07fec84d3b5e6c519fba83c9d2a038f05a
JuanMephisto <adrian@grotter.net>
2022-10-09 17:35:18 +0200

    More work on TransactionHistory.tsx component

--- aksjeapp-frontend/clientapp/src/components/Transactions/TransactionHistory.tsx
+++ aksjeapp-frontend/clientapp/src/components/Transactions/TransactionHistory.tsx
@@ -1,19 +1,57 @@
 import React from "react";
+import TransactionPreview from "./TransactionPreview";
 
 const TransactionHistory = () => {
+    const testTransaction = [
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},
+        {time: "09-10-2022 17:02", price: 105.20, amount: 4800},]
+
+    let counter: number = -1;
     return (
         <>
-            <div>
-                <h1 className="flex justify-center py-1 text-text-display">Transaction history</h1>
-                <div>
-                    <div className={"text-black px-5 grid grid-cols-2"}>
-                        <p className="text-left">Time</p>
-                        <p className="text-left">Price</p>
-                        <p className="text-left">Amount</p>
-                        <p className="text-left">Buyers</p>
-                        <p className="text-left">Sellers</p>
-                    </div>
-                    <hr className="border-text-display"/>
+            <div className="justify-center flex-row px-0">
+                <div className="flex justify-center text-black pb-2">
+                    <h1 className="font-bold">Transaction history</h1>
+                </div>
+                <div className={"text-black grid grid-cols-4 px-5"}>
+                    <p className="text-left col-span-2 ml-10">Time</p>
+                    <p className="text-left">Price</p>
+                    <p className="text-left">Amount</p>
+                </div>
+                <hr className="border-black pb-1 "/>
+                <div className="w-max px-5 h-[14.25rem] scroll max-h-screen overflow-y-auto
+                scrollbar scrollbar-track-white scrollbar-thumb-rounded-3xl scrollbar-thin scrollbar-thumb-blue-700">
+                    {testTransaction.map((val) => {
+                        counter++;
+                        return counter % 2 === 0 ?
+                            <div
+                                className="hover:scale-105 hover:bg-gradient-to-br hover:from-white hover:to-gray-200 hover:rounded-lg rounded-lg transition duration-150 ease-in-out text-stock-preview-text-1 font-semibold">
+                                <TransactionPreview transaction={val}/>
+                            </div> :
+                            <div
+                                className="hover:scale-105 transition duration-150 ease-in-out bg-gradient-to-tl rounded-lg from-green-500 to-blue-700 text-stock-preview-text-2 font-semibold">
+                                <TransactionPreview transaction={val}/>
+                            </div>
+                    })}
                 </div>
             </div>
         </>

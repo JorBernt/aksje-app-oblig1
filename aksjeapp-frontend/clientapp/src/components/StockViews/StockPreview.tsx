a2dadf20c1121364dc2b37426b4c922a79d810e0
jorber <berntsen.jorgen@gmail.com>
2022-10-11 09:19:58 +0200

    Added fetch request with loading and error handling for stock previews Now it will show a loading bar when its waiting for the data, and error if the request fails. This is just an example for now.

--- aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
@@ -1,4 +1,5 @@
 import React, {FC} from 'react';
+import {Stock} from "./StockContainer";
 
 interface PropsObject {
     amount?: number;
@@ -9,24 +10,17 @@ interface PropsObject {
 }
 
 type Props = {
-    items: PropsObject;
+    items: Stock;
     showAmount: boolean;
 }
 
 const StockPreview : FC<Props> = (props: Props) => {
-    const redOrGreen = props.items.difference > 0 ?
-        <p className="py-1 text-green-500 text-center">{"+" + props.items.difference + "%"}</p> :
-        <p className="py-1 text-red-500 text-center">{props.items.difference + "%"}</p>
-    const ifAmount = props.showAmount ? <p className="py-1 text-center">{props.items.amount}</p> : null
+
 
     const cols = "grid w-96 " + (props.showAmount ? "grid-cols-5" : "grid-cols-4")
     return (
         <div className={cols}>
-            <p className="py-1 pl-4 text-center">{props.items.name}</p>
-            {ifAmount}
-            <p className="py-1 text-center">{props.items.chart}</p>
-            {redOrGreen}
-            <p className="py-1 pr-4 text-center">{"$" + props.items.value}</p>
+            <p className="py-1 pl-4 text-center">{props.items.symbol}</p>
         </div>
     );
 }

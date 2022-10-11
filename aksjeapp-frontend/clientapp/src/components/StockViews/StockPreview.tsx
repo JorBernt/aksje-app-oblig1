dc322352948b13813f09180a02e36c3d376e10cb
JuanMephisto <adrian@grotter.net>
2022-10-11 14:37:05 +0200

    Added title to div so full name of stock is displayed when hovering over the div

--- aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
@@ -17,7 +17,7 @@ const StockPreview : FC<Props> = (props: Props) => {
     const cols = "grid w-96 " + (props.showAmount ? "grid-cols-5" : "grid-cols-4")
     return (
         <>
-            <div className={cols} onClick={handleOnClick}>
+            <div className={cols} onClick={handleOnClick} title={props.items.name}>
                 <p className="py-1 pl-4 text-center">{props.items.symbol}</p>
             </div>
 

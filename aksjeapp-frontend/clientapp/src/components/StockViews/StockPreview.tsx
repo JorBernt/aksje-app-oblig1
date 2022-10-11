ff417a54f8a27f2ab9c3d1e9e2200b2f1368a2b0
JuanMephisto <adrian@grotter.net>
2022-10-11 13:47:16 +0200

    Made StockPreview clickable

--- aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
@@ -1,5 +1,6 @@
 import React, {FC} from 'react';
 import {Stock} from "../models";
+import {useNavigate} from "react-router-dom";
 
 type Props = {
     items: Stock;
@@ -8,10 +9,15 @@ type Props = {
 
 const StockPreview : FC<Props> = (props: Props) => {
 
+    let navigate = useNavigate();
+    const handleOnClick = () => {
+        navigate(`/singleStock?symbol=${props.items.symbol}&name=${props.items.name}`)
+    }
+
     const cols = "grid w-96 " + (props.showAmount ? "grid-cols-5" : "grid-cols-4")
     return (
         <>
-            <div className={cols}>
+            <div className={cols} onClick={handleOnClick}>
                 <p className="py-1 pl-4 text-center">{props.items.symbol}</p>
             </div>
 

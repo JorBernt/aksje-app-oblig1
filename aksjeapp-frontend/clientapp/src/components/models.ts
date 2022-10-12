a4e68c4517430dfd7893288b220cce96df58d56a
JuanMephisto <adrian@grotter.net>
2022-10-12 12:19:35 +0200

    Changed stock model

--- aksjeapp-frontend/clientapp/src/components/models.ts
+++ aksjeapp-frontend/clientapp/src/components/models.ts
@@ -1,7 +1,7 @@
 export interface Stock {
     amount?: number;
-    country: string;
-    name: string;
-    sector: string;
     symbol: string;
+    name: string;
+    change: number;
+    value: string;
 }

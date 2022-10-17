b83f50587ca859bc2a7900d8893021a4ce3ac0d3
JuanMephisto <adrian@grotter.net>
2022-10-17 16:07:21 +0200

    Completed Constants.ts and added Transaction to models.ts

--- aksjeapp-frontend/clientapp/src/Constants.ts
+++ aksjeapp-frontend/clientapp/src/Constants.ts
@@ -1,15 +1,44 @@
+import {Transaction} from "./components/models";
+
 const url = "https://localhost:7187/Stocks/"
 
 export const API = {
     GET_ALL_STOCKS: url + "GetAllStocks",
     GET_STOCK_PRICES:
         (symbol: string, fromDate: string, toDate: string) => {
-            return `StockPrices?symbol+=${symbol}&fromDate=${fromDate}&toDate=${toDate}`
+            return url + `StockPrices?symbol+=${symbol}&fromDate=${fromDate}&toDate=${toDate}`
         },
-    BUY_STOCK: url + "BuyStock",
-    SELL_STOCK: url + "SellStock",
+    BUY_STOCK:
+        (socialSecurityNumber: string, symbol: string, number: number) => {
+            return url + `BuyStock?socialSecurityNumber=${socialSecurityNumber}&symbol=${symbol}&number=${number}`
+        },
+    SELL_STOCK:
+        (socialSecurityNumber: string, symbol: string, number: number) => {
+            return url + `SellStock?socialSecurityNumber=${socialSecurityNumber}&symbol=${symbol}&number=${number}`
+        },
     SEARCH_RESULTS:
         (keyPhrase: string) => {
             return url + "SearchResults?keyPhrase=" + keyPhrase;
-        }
+        },
+    GET_ALL_TRANSACTIONS:
+        (socialSecurityNumber: string) => {
+            return url + "GetAllTransactions?socialSecurityNumber=" + socialSecurityNumber
+        },
+    GET_TRANSACTION:
+        (socialSecurityNumber: string, id: number) => {
+            return url + `GetTransaction?socialSecurityNumber=${socialSecurityNumber}&id=${id}`
+        },
+    UPDATE_TRANSACTION:
+        (transaction: Transaction) => {
+            return url + `GetTransaction?transaction=${transaction}`
+        },
+    DELETE_TRANSACTION:
+        (socialSecurityNumber: string, id: number) => {
+            return url + `DeleteTransaction?socialSecurityNumber=${socialSecurityNumber}&id=${id}`
+        },
+    STOCK_CHANGE:
+        (symbol: string) => {
+            return url + `StockChange?symbol=${symbol}`
+        },
+    GET_STOCK_OVERVIEW: url + "GetStockOverview"
 }
--- aksjeapp-frontend/clientapp/src/components/models.ts
+++ aksjeapp-frontend/clientapp/src/components/models.ts
@@ -5,3 +5,13 @@ export interface Stock {
     change: number;
     value: string;
 }
+
+export interface Transaction {
+    id: number,
+    socialSecurityNumber: string,
+    date: string,
+    symbol: string,
+    amount: number,
+    totalPrice: number,
+    isActive: boolean //Vet ikke om denne må være med?
+}

dce07f2504855dc5a230eadd73390173f73a12ae
henriklarsen28 <henrik.hl@hotmail.com>
2022-10-18 12:17:46 +0200

    La inn JsonProperty

--- aksjeapp-backend/aksjeapp-backend/Models/News/News.cs
+++ aksjeapp-backend/aksjeapp-backend/Models/News/News.cs
@@ -1,8 +1,12 @@
 ﻿
+using Newtonsoft.Json;
+
 namespace aksjeapp_backend.Models.News
 {
     public class News
     {
+
+        [JsonProperty("results")]
         public List<NewsResults> Results { get; set; }
     }
 }

b5f2e4c0ab0cc8deebc873ebb22653d823da532d
jorber <berntsen.jorgen@gmail.com>
2022-10-25 09:09:48 +0200

    StockPreview.tsx now handles color for value change.

--- aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
+++ aksjeapp-frontend/clientapp/src/components/StockViews/StockPreview.tsx
@@ -1,6 +1,7 @@
 import React, {FC} from 'react';
 import {Stock} from "../models";
 import {useNavigate} from "react-router-dom";
+import {colorHandler} from "./SingleStockView";
 
 type Props = {
     items: Stock;
@@ -14,13 +15,17 @@ const StockPreview : FC<Props> = (props: Props) => {
         navigate(`/singleStock?symbol=${props.items.symbol}`)
     }
 
+    const changeValue = Math.round(props.items.change * 100) / 100;
+
+    const textColor = colorHandler(changeValue)
+
     const className = "grid w-96 " + (props.showAmount ? "grid-cols-5" : "grid-cols-4")
     return (
         <>
             <div className={className} onClick={handleOnClick} title={props.items.name}>
                 <p className="py-1 pl-4 text-center">{props.items.symbol}</p>
                 <p className="py-1 text-center">{"Stonks"}</p>
-                <p className="py-1 text-center">{Math.round(props.items.change * 100) / 100}</p>
+                <p className={"py-1 text-center " + textColor}>{changeValue}</p>
                 <p className="py-1 pr-4 text-center">{props.items.value}</p>
             </div>
 

cacd9d8f45b7709277388229e811119e6f30d042
henriklarsen28 <henrik.hl@hotmail.com>
2022-10-26 15:26:29 +0200

    La inn url i nyheter. Fikset floating points på stock overview

--- aksjeapp-backend/aksjeapp-backend/Models/PortfolioList.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace aksjeapp_backend.Models
-{
-    public class PortfolioList
-    {
-        public string Symbol { get; set; }
-        public int Amount { get; set; }
-    }
-}
--- aksjeapp-backend/aksjeapp-backend/Models/StockOverview.cs
+++ aksjeapp-backend/aksjeapp-backend/Models/StockOverview.cs
@@ -1,10 +1,37 @@
-﻿namespace aksjeapp_backend.Models;
+﻿using Newtonsoft.Json.Linq;
+
+namespace aksjeapp_backend.Models;
 
 public class StockOverview
 {
     public string Symbol { get; set; }
     public string Name { get; set; }
-    public double Change { get; set; }
-    public double Value { get; set; }
     public int Amount { get; set; } = 0;
+
+    private double change;
+    private double value;
+    public double Change
+    {
+        get
+        {
+            return this.change;
+        }
+        set
+        {
+            this.change = Math.Round(value, 2);
+        }
+    }
+
+    public double Value
+    {
+        get
+        {
+            return this.value;
+        }
+        set
+        {
+            this.value = Math.Round(value, 2);
+        }
+    }
+
 }

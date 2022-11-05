beeda313517ed04fea4de27ee5bc312fa841c739
jorber <berntsen.jorgen@gmail.com>
2022-11-05 14:41:36 +0100

    Made InputField.tsx take a forwarded ref
    You can use a normal ref to get the value from the input field.

--- aksjeapp-frontend/clientapp/src/components/UI/Input/InputField.tsx
+++ aksjeapp-frontend/clientapp/src/components/UI/Input/InputField.tsx
@@ -3,18 +3,20 @@ import React from "react";
 type Props = {
     type: string,
     label: string
+    setValue?: any
+    ref?: any
 }
 
-const InputField: React.FC<Props> = (props) => {
+const InputField: React.FC<Props> = React.forwardRef<HTMLInputElement, Props>((props, ref) => {
     return (
         <>
             <div className="flex flex-col justify-between my-2 text-l w-72">
                 <p>{props.label}</p>
                 <input type={props.type} className="bg-transparent border focus:border-pink-500 rounded-2xl  pl-4 py-2"
-                       style={{outline: "none"}}/>
+                       style={{outline: "none"}} ref={ref}/>
             </div>
         </>
     )
-}
+})
 
 export default InputField;

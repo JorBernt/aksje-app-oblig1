8413e0a9d34c46039c7ba232006f2d563488b13a
jorber <berntsen.jorgen@gmail.com>
2022-11-05 11:50:39 +0100

    Moved basic button to custom component

--- aksjeapp-frontend/clientapp/src/components/Pages/LoginPage.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/LoginPage.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import Navbar from "../Navbar/Navbar";
 import Card from "../UI/Card/Card";
 import InputField from "../UI/Input/InputField";
+import Button from "../UI/Input/Button";
 
 const LoginPage = () => {
+    const handleOnClick = () => {
+
+    }
     return (
         <>
             <Navbar/>
@@ -13,10 +17,9 @@ const LoginPage = () => {
                         <h1 className={"text-center text-2xl mb-4"}>Login</h1>
                         <InputField type={"text"} label={"Email"}/>
                         <InputField type={"password"} label={"Password"}/>
-                        <button
-                            className="mt-4 bg-transparent rounded-2xl w-fit bg-gray-300 hover:bg-gradient-to-tl hover:from-gradient-start hover:to-gradient-end text-black font-semibold py-2 px-4 hover:text-white transition duration-300 ease-in-out">
-                            Log in
-                        </button>
+                        <div className="mt-4">
+                            <Button text={"Log in"} onClick={handleOnClick}/>
+                        </div>
                         <div className="mt-4 flex flex-col justify-center items-center">
                             <p>Don't have an account?</p>
                             <a href={"/"} className={"underline decoration-inherit text-blue-500"}>Register here</a>

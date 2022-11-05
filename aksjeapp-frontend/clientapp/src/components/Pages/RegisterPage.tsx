3dd2ad631cca79fd9772e84606056014c43ba855
jorber <berntsen.jorgen@gmail.com>
2022-11-05 14:47:27 +0100

    Continued work on registerpage, implemented fields and refs

--- aksjeapp-frontend/clientapp/src/components/Pages/RegisterPage.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/RegisterPage.tsx
@@ -4,9 +4,31 @@ import Card from "../UI/Card/Card";
 import InputField from "../UI/Input/InputField";
 import Button from "../UI/Input/Button";
 
-const LoginPage = () => {
-    const handleOnClick = () => {
+interface UserData {
+    name: string;
+    email: string;
+    phone: string;
+    ssn: string;
+    password: string;
+}
 
+
+const LoginPage = () => {
+    const nameRef = React.createRef<HTMLInputElement>()
+    const emailRef = React.createRef<HTMLInputElement>()
+    const phoneRef = React.createRef<HTMLInputElement>()
+    const ssnRef = React.createRef<HTMLInputElement>()
+    const passwordRef = React.createRef<HTMLInputElement>()
+    const retypedPasswordRef = React.createRef<HTMLInputElement>()
+    const handleOnClick = () => {
+        const userData: UserData = {
+            name: String(nameRef.current?.value),
+            email: String(emailRef.current?.value),
+            phone: String(phoneRef.current?.value),
+            ssn: String(ssnRef.current?.value),
+            password: String(passwordRef.current?.value)
+        }
+        console.log(userData)
     }
     return (
         <>
@@ -15,18 +37,14 @@ const LoginPage = () => {
                 <Card>
                     <div className="flex flex-col h-fit items-center">
                         <h1 className={"text-center text-2xl mb-4"}>Register</h1>
-                        <InputField type={"text"} label={"Name"}/>
-                        <InputField type={"text"} label={"Email"}/>
-                        <InputField type={"text"} label={"Phone"}/>
-                        <InputField type={"text"} label={"SSN"}/>
-                        <InputField type={"password"} label={"Password"}/>
-                        <InputField type={"password"} label={"Confirm password"}/>
+                        <InputField type={"text"} label={"Name"} ref={nameRef}/>
+                        <InputField type={"text"} label={"Email"} ref={emailRef}/>
+                        <InputField type={"text"} label={"Phone"} ref={phoneRef}/>
+                        <InputField type={"text"} label={"SSN"} ref={ssnRef}/>
+                        <InputField type={"password"} label={"Password"} ref={passwordRef}/>
+                        <InputField type={"password"} label={"Retype password"} ref={retypedPasswordRef}/>
                         <div className="mt-4">
-                            <Button text={"Log in"} onClick={handleOnClick}/>
-                        </div>
-                        <div className="mt-4 flex flex-col justify-center items-center">
-                            <p>Don't have an account?</p>
-                            <a href={"/"} className={"underline decoration-inherit text-blue-500"}>Register here</a>
+                            <Button text={"Create account"} onClick={handleOnClick}/>
                         </div>
                     </div>
                 </Card>

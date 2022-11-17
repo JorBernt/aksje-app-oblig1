eb2fa83d29ad65b3cb48aa1771ea535aa88677c0
jorber <berntsen.jorgen@gmail.com>
2022-11-17 15:40:26 +0100

    Implemented Register dropdown button from login button

--- aksjeapp-frontend/clientapp/src/components/Navbar/NavbarLinkDropdown.tsx
+++ aksjeapp-frontend/clientapp/src/components/Navbar/NavbarLinkDropdown.tsx
@@ -6,6 +6,7 @@ import {useLoggedInContext} from "../../App";
 type Props = {
     links: string[];
     texts: string[];
+    color: string
 }
 
 const NavbarLinkDropDown: React.FC<Props> = (props) => {
@@ -35,7 +36,7 @@ const NavbarLinkDropDown: React.FC<Props> = (props) => {
                 </button>
                 <div>
                     <button
-                        className={"absolute opacity-0 group-hover:opacity-100 pt-0 group-hover:pt-6 z-0 transition-all  bg-gradient-to-t hover:from-red-200 from-gray-300 hover:to-transparent to-transparent ease-in-out p-5 mx-5  hover:text-white hover:rounded-xl rounded-xl hover:shadow-xl hover:shadow-red-200 w-40 transition duration-300 ease-in-out"}
+                        className={"absolute opacity-0 group-hover:opacity-100 pt-0 group-hover:pt-6 z-0 transition-all  bg-gradient-to-t from-gray-300 hover:to-transparent to-transparent ease-in-out p-5 mx-5  hover:text-white hover:rounded-xl rounded-xl hover:shadow-xl w-40 transition duration-300 ease-in-out " + (`hover:from-${props.color}-200 hover:shadow-${props.color}-200`)}
                         onClick={() => handleOnClick(props.links[1])}>
                         <p className="text-xl text-center text-black">{props.texts[1]}</p>
                     </button>

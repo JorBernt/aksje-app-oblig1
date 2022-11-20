f43dae450acb6433fce9ccea2ce94c7d4b2a032d
henriklarsen28 <henrik.hl@hotmail.com>
2022-11-20 12:03:45 +0100

    Removed console writeline in api file. Added one more key

--- aksjeapp-backend/aksjeapp-backend/APIServices/PolygonAPI.cs
+++ aksjeapp-backend/aksjeapp-backend/APIServices/PolygonAPI.cs
@@ -9,7 +9,6 @@ namespace aksjeapp_backend.DAL
 
         public static async Task<StockPrices> GetStockPrices(string symbol, string fromDate, string toDate, int interval)
         {
-
             try
             {
                 using (var client = new HttpClient())
@@ -30,7 +29,7 @@ namespace aksjeapp_backend.DAL
                         {
                             return null;
                         }
-                        Console.WriteLine("API cooldown");
+                        
                         Thread.Sleep(5000);
 
                         return await GetStockPrices(symbol, fromDate, toDate, interval); // Starts over
@@ -69,7 +68,6 @@ namespace aksjeapp_backend.DAL
                     // Checks if the API returns a bad response
                     if (json.Contains("error"))
                     {
-                        Console.WriteLine("API cooldown");
                         Thread.Sleep(5000);
                         return await GetOpenClosePrice(symbol, date); // Starts over
                     }
@@ -82,7 +80,6 @@ namespace aksjeapp_backend.DAL
             }
             catch
             {
-                Console.WriteLine("Cannot getStockPrice");
                 return null;
             }
         }
@@ -108,12 +105,9 @@ namespace aksjeapp_backend.DAL
                     // Checks if the API returns a bad response
                     if (json.Contains("error"))
                     {
-                        Console.WriteLine("API cooldown");
                         Thread.Sleep(5000);
                         return await GetNews(symbol); // Starts over
                     }
-
-                    Console.WriteLine(json);
                     return JsonConvert.DeserializeObject<News>(json);
 
                 }
@@ -121,13 +115,12 @@ namespace aksjeapp_backend.DAL
             }
             catch
             {
-                Console.WriteLine("Cannot get news");
                 return null;
             }
         }
 
         // List with tokens for the Poltgon API
-        public static List<String> polygonKeys = new List<string>() { "C1cckwJuZuvEgVJbCmv42HuUZnJSgjeJ", "uWXhChA2H2mRpH7fCrGH5NebvagOZEBT", "udqqYjjU5_yjzUpSGVpLBaqGv54SWsIY", "rt8cZtaDtDUSPGsC0nPpHKnl9tpGPPld", "ku6Bcu6IthG2r5m4pBV8bozJqhOobJWq", "vmCz3EMpwNkSZH_1ekwUvCpJ_dPR2Zhy", "bUPoDNh4OFrbTkQ6DLpsSweKMZKaCqcG", "hK9nttsunnnZ4WqCz1cWJC6yZ7l4LM2U", "eBe50HwzILuzw_bAEwqCHOnMOHbkVjdu", "G_pBRj9ts_Bcbwzl2vKEbdW_i9_XROND" };
+        public static List<string> polygonKeys = new() { "C1cckwJuZuvEgVJbCmv42HuUZnJSgjeJ", "uWXhChA2H2mRpH7fCrGH5NebvagOZEBT", "udqqYjjU5_yjzUpSGVpLBaqGv54SWsIY", "rt8cZtaDtDUSPGsC0nPpHKnl9tpGPPld", "ku6Bcu6IthG2r5m4pBV8bozJqhOobJWq", "vmCz3EMpwNkSZH_1ekwUvCpJ_dPR2Zhy", "bUPoDNh4OFrbTkQ6DLpsSweKMZKaCqcG", "hK9nttsunnnZ4WqCz1cWJC6yZ7l4LM2U", "eBe50HwzILuzw_bAEwqCHOnMOHbkVjdu", "G_pBRj9ts_Bcbwzl2vKEbdW_i9_XROND", "nPAR1NjKbrPpKmnnXFyRViftAUH8CJZ4"};
 
         public static string PolygonKeys()
         {

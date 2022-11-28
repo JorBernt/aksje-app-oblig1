2d60ea70e66e54e2bf1479a1eee933d1fa5460e5
jorber <berntsen.jorgen@gmail.com>
2022-11-28 13:05:19 +0100

    Edit page: Implemented not logged in redirect

--- aksjeapp-frontend/clientapp/src/components/Pages/EditPage.tsx
+++ aksjeapp-frontend/clientapp/src/components/Pages/EditPage.tsx
@@ -6,6 +6,7 @@ import Button from "../UI/Input/Button";
 import {UserData} from "../models";
 import {API} from "../../Constants";
 import {useNavigate} from "react-router-dom";
+import {useLoggedInContext} from "../../App";
 
 const EditPage = () => {
     const navigate = useNavigate();
@@ -18,6 +19,8 @@ const EditPage = () => {
     const passwordRef = React.createRef<HTMLInputElement>()
 
     const [customerData, setCustomerData] = useState<UserData>();
+
+    const logInContext = useLoggedInContext()
 
     useEffect(() => {
         fetch(API.STOCK.GET_CUSTOMER_DATA, {credentials: 'include',})
@@ -61,32 +64,41 @@ const EditPage = () => {
     return (
         <>
             <Navbar/>
-            <div className="flex justify-center ">
-                <Card>
-                    <div className="flex flex-col h-fit items-center">
-                        <h1 className={"text-center text-2xl mb-4"}>Edit account</h1>
-                        <div className="grid grid-cols-2">
-                            <InputField type={"text"} label={"First Name"} ref={firstNameRef}
-                                        initVal={customerData?.firstName}/>
-                            <InputField type={"text"} label={"Last Name"} ref={lastNameRef}
-                                        initVal={customerData?.lastName}/>
-                            <InputField type={"text"} label={"SSN"} ref={ssnRef}
-                                        initVal={customerData?.socialSecurityNumber}/>
-                            <InputField type={"text"} label={"Address"} ref={addressRef}
-                                        initVal={customerData?.address}/>
-                            <InputField type={"text"} label={"Postal Code"} ref={pCodeRef}
-                                        initVal={customerData?.postalCode}/>
-                            <InputField type={"text"} label={"Postal Address"} ref={pAddressRef}
-                                        initVal={customerData?.postCity}/>
-                            <InputField type={"password"} label={"Password"} ref={passwordRef}
-                                        initVal={customerData?.password}/>
-                        </div>
-                        <div className="mt-4">
-                            <Button text={"Save"} onClick={handleOnClick}/>
-                        </div>
+            {!logInContext.loggedIn ?
+                <>
+                    <div className={"w-screen flex flex-col gap-4 justify-center items-center mt-96"}>
+                        <p className={"text-2xl "}>You are not logged in</p>
+                        <Button text={"Log in"} onClick={() => navigate("/login")}/>
                     </div>
-                </Card>
-            </div>
+                </>
+                :
+                <div className="flex justify-center ">
+                    <Card>
+                        <div className="flex flex-col h-fit items-center">
+                            <h1 className={"text-center text-2xl mb-4"}>Edit account</h1>
+                            <div className="grid grid-cols-2">
+                                <InputField type={"text"} label={"First Name"} ref={firstNameRef}
+                                            initVal={customerData?.firstName}/>
+                                <InputField type={"text"} label={"Last Name"} ref={lastNameRef}
+                                            initVal={customerData?.lastName}/>
+                                <InputField type={"text"} label={"SSN"} ref={ssnRef}
+                                            initVal={customerData?.socialSecurityNumber}/>
+                                <InputField type={"text"} label={"Address"} ref={addressRef}
+                                            initVal={customerData?.address}/>
+                                <InputField type={"text"} label={"Postal Code"} ref={pCodeRef}
+                                            initVal={customerData?.postalCode}/>
+                                <InputField type={"text"} label={"Postal Address"} ref={pAddressRef}
+                                            initVal={customerData?.postCity}/>
+                                <InputField type={"password"} label={"Password"} ref={passwordRef}
+                                            initVal={customerData?.password}/>
+                            </div>
+                            <div className="mt-4">
+                                <Button text={"Save"} onClick={handleOnClick}/>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            }
         </>
     )
 }

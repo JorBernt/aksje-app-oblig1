2537cc7d97a6cad86f0688c7709e87e3bd3f327f
JuanMephisto <adrian@grotter.net>
2022-11-28 11:02:31 +0100

    Moved unittests for RegisterCustomer to StockController.cs

--- aksjeapp-backend/UnitTesting_aksjeapp/UnitTest_StockController.cs
+++ aksjeapp-backend/UnitTesting_aksjeapp/UnitTest_StockController.cs
@@ -1249,5 +1249,59 @@ namespace UnitTesting_aksjeapp
             //Assert
             Assert.Equal("Transaction not deleted", result.Value);
         }
+        
+        [Fact]
+        public async Task RegisterCustomer_Ok()
+        {
+            var customer = new Customer
+            {
+                SocialSecurityNumber = "12345678910",
+                FirstName = "Jorgen",
+                LastName = "Berntsen",
+                Address = "Osloveien 47a",
+                Balance = 0,
+                Transactions = new List<Transaction>(),
+                PostalCode = "1234",
+                PostCity = "Oslo",
+                Portfolio = new Portfolio
+                {
+                    PortfolioId = 0,
+                    SocialSecurityNumber = "12345678910",
+                    StockPortfolio = new List<PortfolioList>(),
+                    Value = 0
+                }
+            };
+
+            MockRep.Setup(k => k.RegisterCustomer(customer)).ReturnsAsync(true);
+            var results = await _stockController.RegisterCustomer(customer) as OkObjectResult;
+            Assert.Equal("Customer registered", results.Value);
+        }
+        [Fact]
+        public async Task RegisterCustomer_Empty()
+        {
+            var customer = new Customer
+            {
+                SocialSecurityNumber = "12345678910",
+                FirstName = "Jorgen",
+                LastName = "Berntsen",
+                Address = "Osloveien 47a",
+                Balance = 0,
+                Transactions = new List<Transaction>(),
+                PostalCode = "1234",
+                PostCity = "Oslo",
+                Portfolio = new Portfolio
+                {
+                    PortfolioId = 0,
+                    SocialSecurityNumber = "12345678910",
+                    StockPortfolio = new List<PortfolioList>(),
+                    Value = 0
+                }
+            };
+
+            MockRep.Setup(k => k.RegisterCustomer(customer)).ReturnsAsync(false);
+            var results = await _stockController.RegisterCustomer(customer) as BadRequestObjectResult;
+            Assert.Equal("Fault in registerCustomer", results.Value);
+        }
+
     }
 }

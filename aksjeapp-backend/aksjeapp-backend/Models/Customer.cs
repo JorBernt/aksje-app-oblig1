816e83cbd7d2f611b4c71ab596200006be7d6f15
henriklarsen28 <henrik.hl@hotmail.com>
2022-11-29 15:08:17 +0100

    Loggerut etter sletting

--- aksjeapp-backend/aksjeapp-backend/Models/Customer.cs
+++ aksjeapp-backend/aksjeapp-backend/Models/Customer.cs
@@ -6,19 +6,15 @@ namespace aksjeapp_backend.DAL
 {
     public class Customer
     {
-        [JsonProperty("SocialSecurityNumber")]
         [RegularExpression(@"^[0-9]{0,11}$")]
         public string? SocialSecurityNumber { get; set; }
         
-        [JsonProperty("FirstName")]
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string? FirstName { get; set; }
         
-        [JsonProperty("LastName")]
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,20}$")]
         public string? LastName { get; set; }
         
-        [JsonProperty("Address")]
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ0-9. \-]{2,50}$")]
         public string? Address { get; set; }
         
@@ -37,11 +33,9 @@ namespace aksjeapp_backend.DAL
 
         public List<Transaction>? Transactions { get; set; }
         
-        [JsonProperty("PostalCode")]
         [RegularExpression(@"^[0-9]{4}$")]
         public string? PostalCode { get; set; }
         
-        [JsonProperty("PostCity")]
         [RegularExpression(@"^[a-zA-ZæøåÆØÅ. \-]{2,50}$")]
         public string? PostCity { get; set; }
 

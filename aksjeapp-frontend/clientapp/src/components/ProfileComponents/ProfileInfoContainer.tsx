5353fe2727327fad54e61f746631e2b362e15636
jorber <berntsen.jorgen@gmail.com>
2022-11-29 15:50:25 +0100

    ProfileInfoContainer: Added cancel button to deposit/withdraw

--- aksjeapp-frontend/clientapp/src/components/ProfileComponents/ProfileInfoContainer.tsx
+++ aksjeapp-frontend/clientapp/src/components/ProfileComponents/ProfileInfoContainer.tsx
@@ -65,6 +65,10 @@ const ProfileInfoContainer: React.FC<Props> = (props) => {
         setInputValue(e.target.value);
     };
 
+    const handleCancelTransactionClick = () => {
+        setAnimate(false)
+    }
+
     return (
         <>
             {
@@ -89,15 +93,25 @@ const ProfileInfoContainer: React.FC<Props> = (props) => {
                                     className={`mt-8 flex justify-center transition-all ${!animate && "opacity-0 duration-300"} ${animate && "opacity-100 duration-1000"}`}>
                                     <div className="flex-row flex">
                                         <div
-                                            className={`${hideInput && "opacity-0 -translate-x-12"} ${!hideInput && "opacity-100"} transition-all duration-300 w-1/2 flex justify-center`}>
+                                            className={`${hideInput && "opacity-0 -translate-x-12"} ${!hideInput && "opacity-100"} transition-all duration-300 w-1/2 flex justify-center mt-1`}>
                                             <input
                                                 className="w-4/5 h-10 bg-transparent focus:border focus:border-pink-500 rounded-2xl pl-4 border border-black text-center"
                                                 type="number" disabled={!animate} onChange={handleUserInput}
                                                 value={inputValue}/>
                                         </div>
                                         <div
-                                            className={`${hideInput && "-translate-x-10"} transition-all duration-300`}>
+                                            className={`${hideInput && "-translate-x-10"} transition-all duration-300 flex flex-row gap-4`}>
                                             <Button text={transactionType} onClick={handleTransactionClick}/>
+                                            <div
+                                                className={`${hideInput && "opacity-0"} bg-gray-300 w-12 rounded-2xl w-fit group hover:bg-gradient-to-tl hover:from-gradient-start hover:to-gradient-end p-2 transition duration-300 ease-in-out`}
+                                                onClick={handleCancelTransactionClick}>
+                                                <svg
+                                                    className="z-20 svg-icon group-hover:fill-white transition-all duration-300 ease-in-out"
+                                                    viewBox="0 0 20 20">
+                                                    <path
+                                                        d="M15.898,4.045c-0.271-0.272-0.713-0.272-0.986,0l-4.71,4.711L5.493,4.045c-0.272-0.272-0.714-0.272-0.986,0s-0.272,0.714,0,0.986l4.709,4.711l-4.71,4.711c-0.272,0.271-0.272,0.713,0,0.986c0.136,0.136,0.314,0.203,0.492,0.203c0.179,0,0.357-0.067,0.493-0.203l4.711-4.711l4.71,4.711c0.137,0.136,0.314,0.203,0.494,0.203c0.178,0,0.355-0.067,0.492-0.203c0.273-0.273,0.273-0.715,0-0.986l-4.711-4.711l4.711-4.711C16.172,4.759,16.172,4.317,15.898,4.045z"></path>
+                                                </svg>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>

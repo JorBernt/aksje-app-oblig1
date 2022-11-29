d229237ebda6f2e69e7745da58c46f81f0b637ac
JuanMephisto <adrian@grotter.net>
2022-11-29 11:46:01 +0100

    Changed import

--- aksjeapp-frontend/clientapp/src/components/UI/Input/InputField.tsx
+++ aksjeapp-frontend/clientapp/src/components/UI/Input/InputField.tsx
@@ -1,4 +1,4 @@
-import {IconButton} from "@material-ui/core";
+import {IconButton} from "@mui/material";
 import React, {useEffect, useState} from "react";
 
 import VisibilityIcon from '@mui/icons-material/Visibility';
